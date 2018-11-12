346417d739376c3101667be562f6cc3906987c39
Marquis Kurt <software@marquiskurt.net>
2018-11-12 11:44:16 -0500

    Fix syntax

--- termina-dev.rb
+++ termina-dev.rb
@@ -1,7 +1,7 @@
 class TerminaDev < Formula
     desc "A fun CLI SUD game written in Swift"
     homepage "https://github.com/TerminaGame/base"
-    url "https://marquiskurt.visualstudio.com/_apis/resources/Containers/1238012?itemPath=drop&$format=zip"
+    url "https://github.com/TerminaGame/homebrew-termina-osx/releases/download/20181111.1/drop.zip"
     sha256 "7c99ece1df51d1a67517f86b5de1ce4c439a718b6a0c4c201f21e46310f28ae2"
     version "1.0.0beta1build20181111.1"
     
