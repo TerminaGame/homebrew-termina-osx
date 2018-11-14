cb778d569b2a511df1fb1105f9821013c2e6df76
Marquis Kurt <software@marquiskurt.net>
2018-11-14 14:10:36 -0500

    Update release

--- termina-dev.rb
+++ termina-dev.rb
@@ -1,9 +1,9 @@
 class TerminaDev < Formula
     desc "Termina is a fun singe-user dungeon game written in the Swift language."
     homepage "https://github.com/TerminaGame/base"
-    url "https://github.com/TerminaGame/homebrew-termina-osx/releases/download/20181114.1/drop.zip"
-    sha256 "a878fb1b099be50a13d9f2d45980776c214248b64b4c2a2afaec577a0541802d"
-    version "1.0.0beta1build20181114.1"
+    url "https://github.com/TerminaGame/homebrew-termina-osx/releases/download/20181114.2/drop.zip"
+    sha256 "18d91cb6a86eb4288942e28b3eef0336b4dba64e92d2ae50176bde80ca4626ac"
+    version "1.0.0beta1build20181114.2"
         
     bottle :unneeded
     
